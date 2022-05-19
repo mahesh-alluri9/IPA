a0a3179f72612546d8f271c2216cf4823741e945
mahesh-alluri9 <maheshbabualluri2000@gmail.com>
2022-05-19 21:29:30 +0530

    test

---
+++ src/main/java/com/mb/IPA/IpaApplication.java
@@ -0,0 +1,13 @@
+package com.mb.IPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mb/IPA/IpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mb.IPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
